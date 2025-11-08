6ee2af3cb1267283ead2e786c04f30e1226176d3
Skalyaev <anguinau@student.42.fr>
2025-11-08 09:24:11 +0100

    fixed p1

--- p1/router_anguinau/docker-start.sh
+++ p1/router_anguinau/docker-start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+ulimit -n 100000
 /usr/lib/frr/zebra -d -F 'traditional' -A '127.0.0.1' -s '90000000'
 /usr/lib/frr/bgpd  -d -F 'traditional' -A '127.0.0.1'
 /usr/lib/frr/ospfd -d -F 'traditional' -A '127.0.0.1'
